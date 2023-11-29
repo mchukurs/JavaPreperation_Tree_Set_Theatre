f45ea860ac9f233df28a39f595fe78904ae3ea30
Matiss Cukurs <cukursmatiss@gmail.com>
2023-11-29 20:47:33 +0200

    added multiseat booking using nested loops and validation of request

--- Tree_Set_Theatre/src/com/chukurs/BookingAgent.java
+++ Tree_Set_Theatre/src/com/chukurs/BookingAgent.java
@@ -6,20 +6,46 @@ public class BookingAgent {
         int totalSeats = 100;
         Theatre rodgersNYC = new Theatre("Richard Rodgers", totalSeats, rows);
         rodgersNYC.printSeatMap();
-        bookSeat(rodgersNYC,'A',4);
-        bookSeat(rodgersNYC,'A',4);
-        bookSeat(rodgersNYC,'D',1);
+        bookSeat(rodgersNYC, 'A', 4);
+        bookSeat(rodgersNYC, 'A', 4);
+        bookSeat(rodgersNYC, 'D', 1);
+        //testing multiple seat booking
+
+        bookSeats(rodgersNYC,4,'B',3,10);
+        bookSeats(rodgersNYC,6,'B','C',3,10);
+        bookSeats(rodgersNYC,4,'B',1,10);
+        bookSeats(rodgersNYC,4,'B','C',1,10);
+        bookSeats(rodgersNYC,1,'B','C',1,10);
+        bookSeats(rodgersNYC,4,'M','Z',1,10);
+        bookSeats(rodgersNYC,10,'A','E',1,10);
     }
 
-public static void bookSeat(Theatre theatre, char row, int seatNo){
-        String seat = theatre.reserveSeat(row,seatNo);
-        if(seat!=null){
+    public static void bookSeat(Theatre theatre, char row, int seatNo) {
+        String seat = theatre.reserveSeat(row, seatNo);
+        if (seat != null) {
             System.out.println("Congratulations! Your reserved seat is " + seat);
             theatre.printSeatMap();
-        }else {
-            System.out.println("Sorry! Unable to reserve "+ row + seatNo);
+        } else {
+            System.out.println("Sorry! Unable to reserve " + row + seatNo);
+        }
+    }
+
+    public static void bookSeats(Theatre theatre, int ticketCount, char minRow, char maxRow, int minSeatNo, int maxSeatNo) {
+
+        var seats = theatre.reserveMultipleSeats(ticketCount, minRow, maxRow, minSeatNo, maxSeatNo);
+        if (seats != null) {
+            System.out.println("Congratulations! Your reserved seats are " + seats);
+            theatre.printSeatMap();
+
+        } else {
+            System.out.println("Sorry! No matching contiguous seats in rows: " + minRow + " - " + maxRow);
+        }
+    }
+
+    //overloaded version to allow searching for seats in the same row
+    public static void bookSeats(Theatre theatre, int ticketCount, char minRow, int minSeatNo, int maxSeatNo) {
+        bookSeats(theatre, ticketCount, minRow, minRow, minSeatNo, maxSeatNo);
+    }
+
+
 }
-
-
-
-}}
--- Tree_Set_Theatre/src/com/chukurs/Theatre.java
+++ Tree_Set_Theatre/src/com/chukurs/Theatre.java
@@ -1,6 +1,7 @@
 package com.chukurs;
 
 import java.util.NavigableSet;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Theatre {
@@ -22,18 +23,85 @@ public class Theatre {
 
     }
 
+    private boolean validate(int seatCount, char minRow, char maxRow, int minSeat, int maxSeat) {
+
+        boolean result = (minSeat > 0 && seatCountPerRow >= seatCount && (maxSeat - minSeat + 1) >= seatCount);
+        result = result && seats.contains(new Seat(minRow, minSeat));
+        if (!result) {
+            System.out.printf("Invalid! %1$d seats between " +
+                            "%2$c[%3$d-%4$d]-%5$c[%3$d-%4$d] Try again",
+                    seatCount, minRow , minSeat,maxSeat, maxRow);
+            //provide info what can be used
+            System.out.printf(": Seat must be between %s and %s%n",
+                    seats.first().seatNumber, seats.last().seatNumber);
+        }
+        return result;
+    }
+
+    public Set<Seat> reserveMultipleSeats(int seatCount, char minRow, char maxRow, int minSeat, int maxSeat) {
+        //to get the last ROW from our seats
+        char lastValid = seats.last().seatNumber.charAt(0);
+        //if requested row is smaller than last then we use that, if it's outside the seat chart then we use the last row.
+        maxRow = (maxRow < lastValid) ? maxRow : lastValid;
+
+        if (!validate(seatCount, minRow, maxRow, minSeat, maxSeat)) {
+            return null;
+        }
+
+        NavigableSet<Seat> selected = null;
+        //here go through each ROW from out seat chart that matched the requirements from agent
+        for (char letter = minRow; letter <= maxRow; letter++) {
+            //get the seats in the row currently looped over
+            NavigableSet<Seat> contiguous = seats.subSet(
+                    new Seat(letter, minSeat),
+                    true,
+                    new Seat(letter, maxSeat),
+                    true);
+            int index = 0;
+            //will get the 1st unreserved seat
+            Seat first = null;
+            for (Seat current : contiguous) {
+                if (current.reserved) {
+                    //reset index and continue with next seat in the set (as not enough unreserved seats found)
+                    index = 0;
+                    continue;
+                }
+                //set first as current when found the very first unreserved
+                first = (index == 0) ? current : first;
+                //check if found seat count is as requested (increment as not reserved)
+                if (++index == seatCount) {
+                    //get the contiguous seats in the row as per requirement
+                    selected = contiguous.subSet(first, true, current, true);
+                    break;
+                }
+            }
+            if (selected != null) {
+                //break when selected is populated with required seats
+                break;
+            }
+        }
+        Set<Seat> reservedSeats = null;
+        if (selected != null) {
+            selected.forEach(s -> s.reserved = true);
+            //create a copy of set
+            reservedSeats = new TreeSet<>(selected);
+        }
+        return reservedSeats;
+    }
+
     public String reserveSeat(char row, int seat) {
+
         Seat requestedSeat = new Seat(row, seat);
         Seat requested = seats.floor(requestedSeat);
         if (requested == null || !requested.seatNumber.equals(requestedSeat.seatNumber)) {
             System.out.println("No such seat: " + requestedSeat);
             System.out.printf(": Seat must be between %s and %s%n", seats.first().seatNumber, seats.last().seatNumber);
 
-        }else {
-            if(!requested.reserved)
-            {requested.reserved = true;
-            return requested.seatNumber;
-            }else {
+        } else {
+            if (!requested.reserved) {
+                requested.reserved = true;
+                return requested.seatNumber;
+            } else {
                 System.out.println("Seat is already reserved! ");
             }
         }
